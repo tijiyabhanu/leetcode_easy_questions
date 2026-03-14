af6e072de29b8e82863ba10687e1ffe7c8692d0f
tijiyabhanu <23kq1a5487@pace.ac.in>
2026-03-14 07:05:01 +0530

    Delete two_sum.py

--- two_sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d={}
-        for index,value in enumerate(nums):
-            rem = target-value
-            if rem in d:
-                return [d[rem],index]
-            d[value]=index
