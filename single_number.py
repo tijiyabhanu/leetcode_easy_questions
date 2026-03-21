39f87887ede420ba3bc5198ba6e34e20f9f868ed
Tijiya_bhanu <23kq1a5487@pace.ac.in>
2026-03-21 18:13:44 +0530

    Create single_number.py

---
+++ single_number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in d:
+            if d[i]==1:
+                return i
+
+        #OPTIMAL APPROACH
+        xor=0
+        for i in nums:
+            xor=xor^i
+        return xor
